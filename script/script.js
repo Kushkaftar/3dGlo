fe06d575a2bd853b307e9a549ff61b71e8936f92
kushkaftar <kushkaftar@gmail.com>
2020-05-15 21:49:51 +0300

    l 19

--- script/script.js
+++ script/script.js
@@ -2,13 +2,14 @@ window.addEventListener("DOMContentLoaded", function () {
     "use strict";
 
     // Timer ...
-    function countTimer(dedLine) {
+
+    const countTimer = (dedLine) => {
 
         let timerHours = document.getElementById("timer-hours"),
             timerMinutes = document.getElementById("timer-minutes"),
             timerSeconds = document.getElementById("timer-seconds");
 
-        function getTimeRemaning() {
+        const getTimeRemaning = () => {
             let dateStop = new Date(dedLine).getTime(),
             dateNow = new Date().getTime(),
             timeRemaining = (dateStop - dateNow) / 1000,
@@ -27,23 +28,85 @@ window.addEventListener("DOMContentLoaded", function () {
             }
 
             return { hours, minutes, seconds, }
-        }
+        };
 
-        function updateClock() {
+        const updateClock = () => {
             let timer = getTimeRemaning();
             timerHours.textContent = timer.hours;
             timerMinutes.textContent = timer.minutes;
             timerSeconds.textContent = timer.seconds;
 
             setInterval(updateClock, 1000);
-        }
+        };
 
         updateClock();
 
 
-    }
+    };
 
     const addNull = (str) => String(str).length > 1 ? str : `0${str}`;
 
-    countTimer("14 may 2020");
+    countTimer("14 july 2019");
+
+    // menu ...
+
+    const toggleMenu = () => {
+
+        const btnMenu = document.querySelector(".menu"),
+            menu = document.querySelector("menu"),
+            btnClose = document.querySelector(".close-btn"),
+            menuItems = menu.querySelectorAll("ul>li");
+
+        const handlerMenu = () => menu.classList.toggle("active-menu");
+
+        btnMenu.addEventListener("click", handlerMenu);
+
+        btnClose.addEventListener("click", handlerMenu);
+
+        menuItems.forEach((el) => el.addEventListener("click", handlerMenu));
+    };
+
+     toggleMenu();
+
+     // popup ...
+
+    const togglePopup = () => {
+
+        const popup = document.querySelector(".popup"),
+            btnPopup = document.querySelectorAll(".popup-btn"),
+            popupClose = document.querySelector(".popup-close");
+
+        const animate = () => {
+            popup.style.position = "relative";
+            // popup.style.left = "45%";
+            popup.style.display = "block";
+            let top = popup.scrollTop,
+            scrollHeight = popup.scrollHeight;
+            console.dir(popup);
+
+            console.dir(document.documentElement.clientHeight);
+            console.dir(document.documentElement.clientWidth);
+            console.log(top);
+            console.log(scrollHeight);
+            let start = Date.now();
+
+            let timer = setInterval(function() {
+                let timePassed = Date.now() - start;
+
+                popup.style.left = timePassed / 5 + 'px';
+
+                if (timePassed > 2000) clearInterval(timer);
+
+            }, 20);
+
+        };
+
+        btnPopup.forEach((el) => {
+            el.addEventListener("click", animate);
+        });
+
+        popupClose.addEventListener("click", () => popup.style.display = "none");
+    };
+
+    togglePopup();
 });

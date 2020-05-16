d07ff8effe294cdbd9a06039cdeccc69b538a02d
kushkaftar <kushkaftar@gmail.com>
2020-05-16 19:48:48 +0300

    l 21

--- script/script.js
+++ script/script.js
@@ -84,7 +84,6 @@ window.addEventListener("DOMContentLoaded", function () {
             el.addEventListener("click", () => {
                 popup.style.display = "block";
                 popupContent.style.position = "relative";
-                //popupContent.style.top = "-400px";
                 let start = Date.now();
 
                 let timer = setInterval(function() {
@@ -122,10 +121,8 @@ window.addEventListener("DOMContentLoaded", function () {
         const tabHeader = document.querySelector(".service-header"),
             tab = tabHeader.querySelectorAll(".service-header-tab"),
             tabContent = document.querySelectorAll(".service-tab");
-        console.log(tabHeader);
 
         const  toggleTabContent = (index) => {
-            console.log("toggleTabContent");
           for (let i = 0; i < tabContent.length; i++) {
               if (index === i) {
                   tab[i].classList.add("active");
@@ -152,4 +149,96 @@ window.addEventListener("DOMContentLoaded", function () {
     };
 
     tabs();
+
+    // slider ...
+
+    const slide = document.querySelectorAll(".portfolio-item");
+
+    const slider = () => {
+        const addDot = () => {
+            const ul = document.querySelector(".portfolio-dots");
+            slide.forEach((e,i) => {
+
+                let lis = "";
+                lis = i === slide.length- 1 ? `<li class=\"${i} dot dot-active\"></li>` : `<li class=\" ${i} dot\"></li>`;
+                console.log(i, lis);
+
+                ul.insertAdjacentHTML("afterbegin", lis);
+
+            });
+            console.log(ul);
+        };
+        addDot();
+
+        const dot = document.querySelectorAll(".dot"),
+            sliderParent = document.querySelector(".portfolio-content");
+
+        let currentSlide = 0,
+            internal;
+
+        console.log(dot);
+
+        const prevSlide = (elem, index, strClass) => elem[index].classList.toggle(strClass);
+
+        const autoPlaySlide = () => {
+
+            prevSlide(slide , currentSlide,"portfolio-item-active");
+            prevSlide(dot, currentSlide, "dot-active");
+
+            currentSlide++;
+
+            if (currentSlide >= slide.length) currentSlide = 0;
+
+            prevSlide(slide , currentSlide,"portfolio-item-active");
+            prevSlide(dot, currentSlide, "dot-active");
+        };
+
+        const startSlide = (second = 3) => internal = setInterval(autoPlaySlide, second * 1000);
+
+        const stopSlide = () => clearInterval(internal);
+
+        sliderParent.addEventListener("click", (evt) => {
+            evt.preventDefault();
+            let  target = evt.target;
+            if (!target.matches(".dot, .portfolio-btn")) return;
+
+            prevSlide(slide , currentSlide,"portfolio-item-active");
+            prevSlide(dot, currentSlide, "dot-active");
+
+            switch (target.getAttribute("class")) {
+                case "portfolio-btn next":
+                    currentSlide++;
+                    break;
+                case "portfolio-btn prev":
+                    currentSlide--;
+                    break;
+                case "dot":
+                    dot.forEach((elem, index) => {
+                        if (elem === target) currentSlide =index;
+                    });
+                    break;
+            }
+
+            currentSlide = currentSlide >= slide.length ? currentSlide = 0 : currentSlide;
+            currentSlide = currentSlide < 0 ? currentSlide = slide.length - 1: currentSlide;
+
+            prevSlide(slide , currentSlide,"portfolio-item-active");
+            prevSlide(dot, currentSlide, "dot-active");
+
+        });
+
+        sliderParent.addEventListener("mouseover", (evt) => {
+            if (evt.target.matches(".dot, .portfolio-btn")) stopSlide();
+        });
+
+        sliderParent.addEventListener("mouseout", evt => {
+            if (evt.target.matches(".dot, .portfolio-btn")) startSlide();
+        });
+
+        startSlide(1.5);
+
+    };
+
+    slider();
+
 });

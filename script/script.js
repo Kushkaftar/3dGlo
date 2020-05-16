6819b7ac07449699e7adced6eab76bef826f33aa
kushkaftar <kushkaftar@gmail.com>
2020-05-16 12:09:15 +0300

    l 20

--- script/script.js
+++ script/script.js
@@ -3,67 +3,69 @@ window.addEventListener("DOMContentLoaded", function () {
 
     // Timer ...
 
-    const countTimer = (dedLine) => {
-
-        let timerHours = document.getElementById("timer-hours"),
-            timerMinutes = document.getElementById("timer-minutes"),
-            timerSeconds = document.getElementById("timer-seconds");
-
-        const getTimeRemaning = () => {
-            let dateStop = new Date(dedLine).getTime(),
-            dateNow = new Date().getTime(),
-            timeRemaining = (dateStop - dateNow) / 1000,
-                seconds,
-                minutes,
-                hours;
-
-            if (dateNow < dateStop) {
-                seconds = addNull(Math.floor(timeRemaining % 60));
-                minutes = addNull(Math.floor(timeRemaining /60 % 60));
-                hours = addNull(Math.floor(timeRemaining / 60 / 60));
-            } else {
-                hours = "00";
-                minutes = "00";
-                seconds = "00";
-            }
-
-            return { hours, minutes, seconds, }
-        };
-
-        const updateClock = () => {
-            let timer = getTimeRemaning();
-            timerHours.textContent = timer.hours;
-            timerMinutes.textContent = timer.minutes;
-            timerSeconds.textContent = timer.seconds;
-
-            setInterval(updateClock, 1000);
-        };
-
-        updateClock();
-
-
-    };
+    // const countTimer = (dedLine) => {
+    //
+    //     let timerHours = document.getElementById("timer-hours"),
+    //         timerMinutes = document.getElementById("timer-minutes"),
+    //         timerSeconds = document.getElementById("timer-seconds");
+    //
+    //     const getTimeRemaning = () => {
+    //         let dateStop = new Date(dedLine).getTime(),
+    //         dateNow = new Date().getTime(),
+    //         timeRemaining = (dateStop - dateNow) / 1000,
+    //             seconds,
+    //             minutes,
+    //             hours;
+    //
+    //         if (dateNow < dateStop) {
+    //             seconds = addNull(Math.floor(timeRemaining % 60));
+    //             minutes = addNull(Math.floor(timeRemaining /60 % 60));
+    //             hours = addNull(Math.floor(timeRemaining / 60 / 60));
+    //         } else {
+    //             hours = "00";
+    //             minutes = "00";
+    //             seconds = "00";
+    //         }
+    //
+    //         return { hours, minutes, seconds, }
+    //     };
+    //
+    //     const updateClock = () => {
+    //         let timer = getTimeRemaning();
+    //         timerHours.textContent = timer.hours;
+    //         timerMinutes.textContent = timer.minutes;
+    //         timerSeconds.textContent = timer.seconds;
+    //
+    //         setInterval(updateClock, 1000);
+    //     };
+    //
+    //     updateClock();
+    //
+    //
+    // };
 
     const addNull = (str) => String(str).length > 1 ? str : `0${str}`;
 
-    countTimer("14 july 2019");
+    //countTimer("14 july 2019");
 
     // menu ...
 
     const toggleMenu = () => {
 
         const btnMenu = document.querySelector(".menu"),
-            menu = document.querySelector("menu"),
-            btnClose = document.querySelector(".close-btn"),
-            menuItems = menu.querySelectorAll("ul>li");
+            menu = document.querySelector("menu");
 
         const handlerMenu = () => menu.classList.toggle("active-menu");
 
         btnMenu.addEventListener("click", handlerMenu);
 
-        btnClose.addEventListener("click", handlerMenu);
+        menu.addEventListener("click",(evt) => {
+            let target = evt.target;
 
-        menuItems.forEach((el) => el.addEventListener("click", handlerMenu));
+            if (target.tagName === "A") {
+                handlerMenu();
+            }
+        })
     };
 
      toggleMenu();
@@ -74,39 +76,67 @@ window.addEventListener("DOMContentLoaded", function () {
 
         const popup = document.querySelector(".popup"),
             btnPopup = document.querySelectorAll(".popup-btn"),
-            popupClose = document.querySelector(".popup-close");
+            popupContent = document.querySelector(".popup-content");
 
-        const animate = () => {
-            popup.style.position = "relative";
-            // popup.style.left = "45%";
-            popup.style.display = "block";
-            let top = popup.scrollTop,
-            scrollHeight = popup.scrollHeight;
-            console.dir(popup);
 
-            console.dir(document.documentElement.clientHeight);
-            console.dir(document.documentElement.clientWidth);
-            console.log(top);
-            console.log(scrollHeight);
-            let start = Date.now();
-
-            let timer = setInterval(function() {
-                let timePassed = Date.now() - start;
-
-                popup.style.left = timePassed / 5 + 'px';
-
-                if (timePassed > 2000) clearInterval(timer);
-
-            }, 20);
-
-        };
 
         btnPopup.forEach((el) => {
-            el.addEventListener("click", animate);
+            el.addEventListener("click", () => {
+                popup.style.display = "block"
+            });
         });
 
-        popupClose.addEventListener("click", () => popup.style.display = "none");
+        popup.addEventListener("click", (evt) => {
+            let target = evt.target;
+
+            if (target.classList.contains("popup-close")) {
+                popup.style.display = "none";
+            } else  {
+                target = target.closest(".popup-content");
+                if (!target) {
+                    popup.style.display = "none";
+                }
+            }
+
+        });
     };
 
     togglePopup();
+
+    // tabs ...
+
+    const tabs = () => {
+        const tabHeader = document.querySelector(".service-header"),
+            tab = tabHeader.querySelectorAll(".service-header-tab"),
+            tabContent = document.querySelectorAll(".service-tab");
+        console.log(tabHeader);
+
+        const  toggleTabContent = (index) => {
+            console.log("toggleTabContent");
+          for (let i = 0; i < tabContent.length; i++) {
+              if (index === i) {
+                  tab[i].classList.add("active");
+                  tabContent[i].classList.remove("d-none");
+              } else {
+                  tab[i].classList.remove("active");
+                  tabContent[i].classList.add("d-none");
+              }
+          }
+        };
+
+        tabHeader.addEventListener("click", (evt) => {
+            let target = evt.target;
+            target = target.closest(".service-header-tab");
+            if (target) {
+                tab.forEach((item, i) => {
+                    if (item === target) {
+                        toggleTabContent(i);
+                    }
+                })
+            }
+
+        })
+    };
+
+    tabs();
 });

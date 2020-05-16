3d7dde50831414619e2fdd4c3c78a98c7b68be91
kushkaftar <kushkaftar@gmail.com>
2020-05-16 23:44:31 +0300

    l 20 add timer

--- script/script.js
+++ script/script.js
@@ -3,50 +3,48 @@ window.addEventListener("DOMContentLoaded", function () {
 
     // Timer ...
 
-    // const countTimer = (dedLine) => {
-    //
-    //     let timerHours = document.getElementById("timer-hours"),
-    //         timerMinutes = document.getElementById("timer-minutes"),
-    //         timerSeconds = document.getElementById("timer-seconds");
-    //
-    //     const getTimeRemaning = () => {
-    //         let dateStop = new Date(dedLine).getTime(),
-    //         dateNow = new Date().getTime(),
-    //         timeRemaining = (dateStop - dateNow) / 1000,
-    //             seconds,
-    //             minutes,
-    //             hours;
-    //
-    //         if (dateNow < dateStop) {
-    //             seconds = addNull(Math.floor(timeRemaining % 60));
-    //             minutes = addNull(Math.floor(timeRemaining /60 % 60));
-    //             hours = addNull(Math.floor(timeRemaining / 60 / 60));
-    //         } else {
-    //             hours = "00";
-    //             minutes = "00";
-    //             seconds = "00";
-    //         }
-    //
-    //         return { hours, minutes, seconds, }
-    //     };
-    //
-    //     const updateClock = () => {
-    //         let timer = getTimeRemaning();
-    //         timerHours.textContent = timer.hours;
-    //         timerMinutes.textContent = timer.minutes;
-    //         timerSeconds.textContent = timer.seconds;
-    //
-    //         setInterval(updateClock, 1000);
-    //     };
-    //
-    //     updateClock();
-    //
-    //
-    // };
+    const countTimer = (dedLine) => {
+
+        let timerHours = document.getElementById("timer-hours"),
+            timerMinutes = document.getElementById("timer-minutes"),
+            timerSeconds = document.getElementById("timer-seconds");
+
+        const getTimeRemaning = () => {
+            let dateStop = new Date(dedLine).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+                seconds,
+                minutes,
+                hours;
+
+            if (dateNow < dateStop) {
+                seconds = addNull(Math.floor(timeRemaining % 60));
+                minutes = addNull(Math.floor(timeRemaining /60 % 60));
+                hours = addNull(Math.floor(timeRemaining / 60 / 60));
+            } else {
+                hours = "00";
+                minutes = "00";
+                seconds = "00";
+            }
+
+            return { hours, minutes, seconds, }
+        };
+
+        const updateClock = () => {
+            let timer = getTimeRemaning();
+            timerHours.textContent = timer.hours;
+            timerMinutes.textContent = timer.minutes;
+            timerSeconds.textContent = timer.seconds;
+
+
+        };
+        setInterval(updateClock, 1000);
+
+    };
 
     const addNull = (str) => String(str).length > 1 ? str : `0${str}`;
 
-    //countTimer("14 july 2019");
+    countTimer("14 july 2019");
 
     // menu ...
 
